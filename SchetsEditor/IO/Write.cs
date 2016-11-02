087044b68dd950b3343d999957bd2846b2eb76d1
Wilmer Zwietering <wzwietering@gmail.com>
2016-11-02 15:32:13 +0100

    Write function, not fully functional

---
+++ SchetsEditor/IO/Write.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SchetsEditor.IO
+{
+    class Write
+    {
+        /// <summary>
+        /// Maakt een csv file met alle data van de tekening
+        /// </summary>
+        /// <param name="path">De locatie om het bestand op te slaan</param>
+        /// <param name="objects">De objecten die opgeslagen moeten worden</param>
+        public void WriteCSV(string path, List<DrawnItem> objects)
+        {
+            StreamWriter fs = new StreamWriter(path);
+            foreach(DrawnItem d in objects)
+            {
+                string line = d.color.ToString();
+
+                foreach(DrawnElement de in d.elements)
+                {
+                    line = line + "," + de.GetType().ToString() + "," + de.pointA + "," + de.pointB + "," + de.LineThickness;
+                }
+                fs.WriteLine(line);
+            }
+            fs.Close();
+        }
+    }
+}
