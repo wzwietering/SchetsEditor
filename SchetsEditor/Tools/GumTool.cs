10e034e0ba3669d768a473fdf1ee7812d46a5cb8
Maaike <maaikegalama@outlook.com>
2016-11-09 10:18:53 +0100

    Added a selector tool

--- SchetsEditor/Tools/GumTool.cs
+++ SchetsEditor/Tools/GumTool.cs
@@ -5,20 +5,18 @@ using System.Linq;
 namespace SchetsEditor
 {
     // With this tool you can erase an element from the list of drawnItems.
-    public class GumTool : OneDimensionalTool
+    public class GumTool : SelectorTool
     {
         public override string ToString() { return "gum"; }
 
         public override void MuisLos(SchetsControl s, Point p)
         {
-            // Get the object that was clicked
-            var clickedObjects = s.Schets.GetDrawnItems().Where(o => o.elements.Any(e => e.WasClicked(p)));
-            if (clickedObjects != null && clickedObjects.Count() > 0)
+            base.MuisLos(s, p);
+         
+            if (this.selectedItem != null)
             {
-                var clickedObject = clickedObjects.Last();
-
                 // remove it and rebuild the bitmap.
-                s.Schets.RemoveElement(clickedObject);
+                s.Schets.RemoveElement(selectedItem);
                 s.RebuildBitmap(this, new EventArgs());
             }
         }
