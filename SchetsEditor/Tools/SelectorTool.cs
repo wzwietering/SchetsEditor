eb7dfedb5a7995eb55ad352beb3f93b7d836bfcb
Maaike <maaikegalama@outlook.com>
2016-11-09 10:58:50 +0100

    selecrtool changed

--- SchetsEditor/Tools/SelectorTool.cs
+++ SchetsEditor/Tools/SelectorTool.cs
@@ -14,6 +14,9 @@ namespace SchetsEditor
 
         public override void MuisLos(SchetsControl s, Point p)
         {
+            // Reset the selected item (because the user made a new click)
+            this.selectedItem = null;
+
             // Get the object that was clicked
             var clickedObjects = s.Schets.drawnItems.Where(o => o.elements.Any(e => e.WasClicked(p)));
             if (clickedObjects != null && clickedObjects.Count() > 0)
